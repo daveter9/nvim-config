0470871351a7e77b18f6e34f278d8f8a57178e9b
dlamoolenaar <david@modare.nl>
2023-12-30 11:49:04 +0100

    Init config

---
+++ lua/dlamoolenaar/init.lua
@@ -0,0 +1,17 @@
+require("dlamoolenaar.load_lazy")
+require("dlamoolenaar.remap")
+
+require("lazy").setup({
+	{
+		'nvim-telescope/telescope.nvim', 
+		tag = '0.1.5',
+		dependencies = { 'nvim-lua/plenary.nvim' }
+	},
+	{ 'rose-pine/neovim', name = 'rose-pine' }
+})
+
+require("rose-pine").setup({
+	variant = "main"
+})
+
+vim.cmd("colorscheme rose-pine")
